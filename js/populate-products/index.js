f9a0794768b6a2be2f9d499414b94b93d6bdb907
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2025-09-12 19:42:43 -0300

    feat: add all products

--- js/populate-products/index.js
+++ js/populate-products/index.js
@@ -13,13 +13,76 @@ const popularProducts = [
 
 const allProducts = [...popularProducts];
 
-allProducts.push();// implementar depois mais produtos;
-
+allProducts.push(
+    {
+        name: "Running canvas",
+        price: "2999.00",
+        img: "../assets/images/shoes/canva-shoes.png",
+        gender: "masculino",
+        quantity: 10
+    },
+    {
+        name: "Running casual",
+        price: "3999.00",
+        img: "../assets/images/shoes/casual.png",
+        gender: "masculino",
+        quantity: 15
+    },
+    {
+        name: "Casual Nile",
+        price: "3999.00",
+        img: "../assets/images/shoes/nike.png",
+        gender: "feminino",
+        quantity: 12
+    },
+    {
+        name: "Trail Runner",
+        price: "4999.00",
+        img: "https://images.pexels.com/photos/19090/pexels-photo.jpg",
+        gender: "masculino",
+        quantity: 8
+    },
+    {
+        name: "Street Sneaker",
+        price: "3499.00",
+        img: "https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg",
+        gender: "feminino",
+        quantity: 20
+    },
+    {
+        name: "Classic Leather",
+        price: "4599.00",
+        img: "https://images.pexels.com/photos/267202/pexels-photo-267202.jpeg",
+        gender: "masculino",
+        quantity: 5
+    },
+    {
+        name: "Sporty Flex",
+        price: "3799.00",
+        img: "https://images.pexels.com/photos/2529149/pexels-photo-2529149.jpeg",
+        gender: "masculino",
+        quantity: 18
+    },
+    {
+        name: "Urban Runner",
+        price: "4299.00",
+        img: "https://images.pexels.com/photos/2529150/pexels-photo-2529150.jpeg",
+        gender: "feminino",
+        quantity: 7
+    },
+    {
+        name: "Urban Runner",
+        price: "4299.00",
+        img: "https://images.pexels.com/photos/2529150/pexels-photo-2529150.jpeg",
+        gender: "feminino",
+        quantity: 9
+    }
+);
 
 const mostPopularContainer = document.getElementById("popular-products");
+const allProductsContainer = document.getElementById("all-products");
 const templateProduct = document.getElementById("template-product-card");
 
-console.log(templateProduct);
 
 popularProducts.forEach((product) => {
     const copy = templateProduct.content.cloneNode(true);
@@ -28,4 +91,14 @@ popularProducts.forEach((product) => {
     copy.querySelector("p").textContent = product.price;
 
     mostPopularContainer.appendChild(copy);
-})
+});
+
+allProducts.forEach((product)=>{
+    const copy = templateProduct.content.cloneNode(true);
+    copy.querySelector("h3").textContent = product.name;
+    copy.querySelector("img").src = product.img;
+    copy.querySelector("p").textContent = product.price;
+
+    allProductsContainer.appendChild(copy);
+
+});
